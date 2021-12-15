76c04c88f3facd74882a046983a167f941cc90e6
Ole Melhus <ole@on-it.no>
2021-12-15 11:23:46 +0100

    initial commit

---
+++ VismanetAuthControl/VismaNetAuthControl.xaml.cs
@@ -0,0 +1,113 @@
+﻿using ONIT.VismaNetApi;
+using System;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace VismanetWPF
+{
+    /// <summary>
+    /// Interaction logic for VismaNetAuthControl.xaml
+    /// </summary>
+    public partial class AuthControl : UserControl
+    {
+        private string callbackUrl;
+        private string clientId;
+
+        public string CallbackUrl { get => callbackUrl; set { callbackUrl = value; LoadUrl(); } }
+        public string ClientId { get => clientId; set { clientId = value; LoadUrl(); } }
+        public string ClientSecret { get; set; }
+
+        public bool ShowTokenIfSuccessful { get; set; }
+
+        public event EventHandler<string> OnTokenReceived;
+        public event EventHandler<(Exception, string)> OnError;
+        public AuthControl()
+        {
+            InitializeComponent();
+            Loaded += AuthControl_Loaded;
+        }
+
+        private void LoadUrl()
+        {
+            if (!string.IsNullOrEmpty(ClientId) && !string.IsNullOrEmpty(CallbackUrl))
+                vnBrowser.Source = new Uri(VismaNet.GetOAuthUrl(ClientId, CallbackUrl));
+        }
+
+        private void AuthControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            LoadUrl();
+            vnBrowser.NavigationStarting += VnBrowser_NavigationStarting;
+        }
+
+        private async void VnBrowser_NavigationStarting(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e)
+        {
+            try
+            {
+                var query = new Uri(e.Uri).Query.TrimStart('?')
+                    .Split(new[] { '&' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(k => k.Split('='))
+                    .Where(k => k.Length == 2)
+                    .ToLookup(a => a[0], a => Uri.UnescapeDataString(a[1])
+                      , StringComparer.OrdinalIgnoreCase);
+
+                var code = query["code"].FirstOrDefault();
+                if (query != null && !string.IsNullOrEmpty(code))
+                {
+                    vnBrowser.NavigateToString(CreateTemplatedResult("Fetching token from Visma.net...", "Fetching token from Visma.net. Please wait..."));
+                    var token = await VismaNet.GetTokenUsingOAuth(ClientId, ClientSecret, code, CallbackUrl);
+                    if (token != null)
+                    {
+                        vnBrowser.NavigateToString(CreateTemplatedResult("Token fetched from Visma.net", "The token was successfully fetched from Visma.net." + (ShowTokenIfSuccessful == true ? $"<br/><br/>Token: {token}" : string.Empty), "green darken-2"));
+                        OnTokenReceived?.Invoke(this, token);
+                        return;
+                    }
+                    else
+                    {
+                        vnBrowser.NavigateToString(CreateTemplatedResult("Token fetch failed...", "We have no idea why...", "red darken-2"));
+                        OnError?.Invoke(this, (null, "Token fetch failed"));
+                    }
+                }
+                else
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                vnBrowser.NavigateToString(CreateTemplatedResult("Token fetch failed...", ex.Message, "red darken-2"));
+                OnError?.Invoke(this, (ex, ex.Message));
+            }
+        }
+
+        private static string CreateTemplatedResult(string title, string content,
+          string background = "blue-grey darken-1")
+        {
+            return "<html>" +
+                    "<head>" +
+                    "<link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">" +
+                    "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css\" integrity=\"sha256-OweaP/Ic6rsV+lysfyS4h+LM6sRwuO3euTYfr6M124g=\" crossorigin=\"anonymous\" />" +
+                    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>" +
+                    "<title>Visma.net Integrations Token Generator</title>" +
+                    "</head>" +
+                    "<body>" +
+                    "<div class=\'container\'>" +
+                    "" +
+                    "<div class=\'row\'>" +
+                    "<div class=\'col s12 m8 offset-m2\'>" +
+                    $"<div class='card {background} z-depth-5'>" +
+                    "<div class=\'card-content white-text\'>" +
+                    $"<span class='card-title'>{title}</span>" +
+                    $"{content}" +
+                    "</div>" +
+                    "</div>" +
+                    "<a href=\'https://on-it.no\' target=\'_blank\'>" +
+                    "<img src=\"https://www.on-it.no/wp-content/themes/on-it/style/images/on_it_logo.png\" width=\"100px\" class=\'right\' />" +
+                    "</a>" +
+                    "</div>" +
+                    "</div>" +
+                    "" +
+                    "</div>" +
+                    "</body>" +
+                    "</html>";
+        }
+    }
+}
---
+++ VismanetWPFAuth/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using VismanetWPFAuth.Properties;
+
+namespace VismanetWPFAuth
+{
+    public static class EncryptionHelper
+    {
+        public static string EncryptString(string input, string key)
+        {
+            byte[] encryptedData = System.Security.Cryptography.ProtectedData.Protect(
+                System.Text.Encoding.Unicode.GetBytes(input),
+                Encoding.UTF8.GetBytes(key),
+                System.Security.Cryptography.DataProtectionScope.CurrentUser);
+            return Convert.ToBase64String(encryptedData);
+        }
+
+        public static string DecryptString(string encryptedData, string key)
+        {
+            try
+            {
+                byte[] decryptedData = System.Security.Cryptography.ProtectedData.Unprotect(
+                    Convert.FromBase64String(encryptedData),
+                    Encoding.UTF8.GetBytes(key),
+                    System.Security.Cryptography.DataProtectionScope.CurrentUser);
+                return Encoding.Unicode.GetString(decryptedData);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static Action<T> Debounce<T>(this Action<T> func, int milliseconds = 300)
+        {
+            CancellationTokenSource? cancelTokenSource = null;
+
+            return arg =>
+            {
+                cancelTokenSource?.Cancel();
+                cancelTokenSource = new CancellationTokenSource();
+
+                Task.Delay(milliseconds, cancelTokenSource.Token)
+                    .ContinueWith(t =>
+                    {
+                        if (t.IsCompletedSuccessfully)
+                        {
+                            func(arg);
+                        }
+                    }, TaskScheduler.Default);
+            };
+        }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            VismaNetControl.ClientId = Settings.Default.ClientId;
+            VismaNetControl.CallbackUrl = Settings.Default.CallbackUrl;
+            if (!string.IsNullOrEmpty(Settings.Default.ClientSecret))
+            {
+                try
+                {
+                    VismaNetControl.ClientSecret = EncryptionHelper.DecryptString(Settings.Default.ClientSecret, Settings.Default.ClientId);
+                    InputClientSecret.Password = VismaNetControl.ClientSecret;
+                }
+                catch { }
+            }
+            InputClientId.Text = VismaNetControl.ClientId;
+            InputCallbackUrl.Text = VismaNetControl.CallbackUrl;
+        }
+
+        private void AuthControl_OnTokenReceived(object sender, string e)
+        {
+            try
+            {
+                Clipboard.SetText(e);
+            }
+            catch (Exception) { }
+            MessageBox.Show($"Token: {e}");
+        }
+
+        private void ClientSecret_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            VismaNetControl.ClientSecret = InputClientSecret.Password;
+            Settings.Default.ClientSecret = EncryptionHelper.EncryptString(VismaNetControl.ClientSecret, VismaNetControl.ClientId);
+            Settings.Default.Save();
+        }
+
+        private void SaveSettings_Click(object sender, RoutedEventArgs e)
+        {
+            VismaNetControl.ClientId = InputClientId.Text;
+            VismaNetControl.CallbackUrl = InputCallbackUrl.Text;
+            VismaNetControl.ClientSecret = InputClientSecret.Password;
+            Settings.Default.ClientId = VismaNetControl.ClientId;
+            Settings.Default.ClientSecret = EncryptionHelper.EncryptString(VismaNetControl.ClientSecret, VismaNetControl.ClientId);
+            Settings.Default.CallbackUrl = VismaNetControl.CallbackUrl;
+            Settings.Default.Save();
+        }
+    }
+}
